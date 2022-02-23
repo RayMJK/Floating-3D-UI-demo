dac2993e044025bfd135ab53aed8fca4bb0d4326
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-02-23 19:18:57 +0900

    Create main.js

---
+++ frontend/main.js
@@ -0,0 +1,11 @@
+
+function play() { 
+    var audio = document.getElementById('audio_play'); 
+    if (audio.paused) { 
+        audio.play(); 
+    }else{ 
+        audio.pause(); 
+        audio.currentTime = 0 
+    } 
+} 
+    
